662160f2029c27240572ed03d4b2b61dbf123d9d
xuyang.zhai <96709238+Zhaixuyang11@users.noreply.github.com>
2023-10-23 11:26:21 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig4CustomTheme({
       description: '前端编码规范工程化',
     },
   },
-  base: '/fe-spec/',
+  base: '/fe-code/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/index.md' },
@@ -110,13 +110,13 @@ export default defineConfig4CustomTheme({
     footer: {
       createYear: 2023,
       copyrightInfo:
-        'encode studio | <a href="https://github.com/encode-studio-fe/fe-spec" target="_blank">github</a>',
+        'encode studio | <a href="https://githubfast.com/encode-studio-fe/fe-spec" target="_blank">github</a>',
     },
 
     extendFrontmatter: {
       author: {
         name: '澄怀',
-        link: 'https://github.com/encode-studio-fe/fe-sepc',
+        link: 'https://githubfast.com/encode-studio-fe/fe-sepc',
       },
     },
   },
